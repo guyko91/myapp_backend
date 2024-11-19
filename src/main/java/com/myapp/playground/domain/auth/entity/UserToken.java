5a1ec03c87b60a31b78150187718c444f2c9397c
larry <guyko91@gmail.com>
2024-11-19 22:04:06 +0900

    [fix] UserToken 레디스 Entity id 필드 추가.

--- src/main/java/com/myapp/playground/domain/auth/entity/UserToken.java
+++ src/main/java/com/myapp/playground/domain/auth/entity/UserToken.java
@@ -10,14 +10,17 @@ import org.springframework.data.redis.core.RedisHash;
 public class UserToken implements Serializable {
 
     @Id
-    private String userId;
+    private String id;
+    private Long userId;
     private String accessToken;
     private String refreshToken;
     private long expiration;
 
     protected UserToken() { }
 
-    private UserToken(String userId, String accessToken, String refreshToken, long expiration) {
+    private UserToken(String id, Long userId, String accessToken, String refreshToken,
+        long expiration) {
+        this.id = id;
         this.userId = userId;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
